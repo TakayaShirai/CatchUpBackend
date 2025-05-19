7d758226af20798d4fb214aa6dd429db2825a96f
TakayaShirai <s78taka03@gmail.com>
2025-05-19 16:17:03 +0900

    [Go] Add Testing Section.

--- Go/main.go
+++ Go/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
 	"time"
@@ -255,6 +256,15 @@ func main() {
 	}
 
 	fmt.Println(string(newJson))
+
+	// Testing
+	res, err := divide(100.0, 0)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	log.Println("result of division is", res)
 }
 
 const numPool = 10
@@ -276,4 +286,15 @@ func changeUsingPointer(s *string) {
 func addPartitionBar(s string) {
 	fmt.Println()
 	fmt.Println(s, "----------------------------------------------")
+}
+
+func divide(x, y float32) (float32, error) {
+	var result float32
+
+	if y == 0 {
+		return result, errors.New("cannot divide by 0")
+	}
+
+	result = x / y
+	return result, nil
 }
