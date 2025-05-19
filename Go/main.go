fcd35a5325c55f94e331d21ad8caf700d27fa1ae
TakayaShirai <s78taka03@gmail.com>
2025-05-19 11:18:01 +0900

    [Go] Add Pointers Section.

--- Go/main.go
+++ Go/main.go
@@ -1,8 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+)
 
 func main() {
+	// Variables & Functions
+	addPartitionBar("Variables & Functions")
 	fmt.Println("Hello, World!")
 
 	var whatToSay string = "Goodbye, cruel world!"
@@ -15,8 +20,26 @@ func main() {
 
 	whatWasSaid, theOtherThingThatWasSaid := saySomething()
 	fmt.Println("The function returned", whatWasSaid, theOtherThingThatWasSaid)
+
+	// Pointers
+	addPartitionBar("Pointers")
+	var myString string
+	myString = "Green"
+	log.Println("myString is set to", myString)
+	changeUsingPointer(&myString)
+	log.Println("myString is set to", myString)
 }
 
 func saySomething() (string, string) {
 	return "something", "else"
+}
+
+func changeUsingPointer(s *string) {
+	newValue := "Red"
+	*s = newValue
+}
+
+func addPartitionBar(s string) {
+	fmt.Println()
+	fmt.Println(s, "----------------------------------------------")
 }
