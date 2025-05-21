33f750b2dd2f799b87334f6dd920af847e5c5422
TakayaShirai <s78taka03@gmail.com>
2025-05-21 18:10:09 +0900

    [Go] Enable Go Modules and Refactor code.

--- BasicWebAppBootcamp/cmd/web/main.go
+++ BasicWebAppBootcamp/cmd/web/main.go
@@ -3,14 +3,16 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/TakayaShirai/CatchUpBackend/BasicWebAppBootcamp/pkg/handlers"
 )
 
 const portNumber = ":8080"
 
 // main is the main application function
 func main() {
-	http.HandleFunc("/", Home)
-	http.HandleFunc("/about", About)
+	http.HandleFunc("/", handlers.Home)
+	http.HandleFunc("/about", handlers.About)
 
 	fmt.Println("Starting application on port", portNumber)
 	_ = http.ListenAndServe(portNumber, nil)
